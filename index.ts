2f515fdf05e5cb0e479d2a0dd3432fdcc0df0fee
Fariha-Asif <farihadua70@gmail.com>
2024-03-20 12:32:55 +0500

    Add new some types

--- index.ts
+++ index.ts
@@ -1,16 +1,17 @@
+
 import inquirer from "inquirer";
 import chalk from "chalk";
 
 const answer = await inquirer.prompt([
   {
-    message: chalk.white.bold.italic("Enter first number"),
+    message: ((chalk.white.bold.italic)("Enter first number")),
     type: "number",
-    name: chalk.blue.bold.italic("Number-1"),
+    name: "firstNumber",
   },
   {
-    message: chalk.white.bold.italic("Enter Second number"),
+    message: ((chalk.white.bold.italic)("Enter Second number")),
     type: "number",
-    name: chalk.red.bold.italic("Number-2"),
+    name: "secondNumber",
   },
   {
     message: "Select one of the operator to perform operation",
@@ -26,17 +27,17 @@ const answer = await inquirer.prompt([
 ]);
 
 if (answer.operator === "Addition") {
-  console.log(answer.Number-1 + answer.Number-2);
+  console.log(answer.firstNumber + answer.secondNumber);
 } else if (answer.operator === "Subtraction") {
-  console.log(answer.Number-1 - answer.Number-2);
+  console.log(answer.firstNumber - answer.secondNumber);
 } else if (answer.operator === "Multiplication") {
-  console.log(answer.Number-1 * answer.Number-2);
+  console.log(answer.firstNumber * answer.secondNumber);
 } else if (answer.operator === "Division") {
-  console.log(answer.Number-1 / answer.Number-2);
+  console.log(answer.firstNumber / answer.secondNumber);
 } else if (answer.operator === "Modulus") {
-  console.log(answer.Number-1 % answer.Number-2);
+  console.log(answer.firstNumber % answer.secondNumber);
 } else if (answer.operator === "Exponention") {
-  console.log(answer.Number-1 ** answer.Number-2);
+  console.log(answer.firstNumber ** answer.secondNumber);
 } else {
   console.log("Please Select Valid Operator.");
 }
